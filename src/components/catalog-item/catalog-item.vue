ba91cbc77cc985a7c3ad03fe5e91fa139abd050c
Khrystyna Rypetska <rypetska.h@gmail.com>
2021-09-06 14:45:04 +0300

    first commit

---
+++ src/components/catalog-item/catalog-item.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="item">
+    <h3>{{item_data.position}}</h3>
+    <p>{{item_data.employmentType}}</p>
+    <p>{{item_data.status}}</p>
+    <p>{{item_data.experience}}</p>
+    <p>{{item_data.role}}</p>
+    <p><strong>{{item_data.salaryFrom}}грн - {{item_data.salaryTo}}грн</strong></p>
+    <p>{{item_data.currency}}</p>
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: "catalog-item",
+  props: {
+    item_data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+
+  data () {
+    return  {
+      title : 'Vacancy'
+    }
+  }
+
+}
+</script>
---
+++ src/components/catalog/catalog.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="item-job">
+      <div class="catalog">
+        <CatalogItem/>
+        <CatalogItem
+            v-for="item in data"
+            :key="item.id"
+            :item_data="item"
+        />
+        <div v-for="item in data.items"
+             :key="item.id"
+             :item_data="item">
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import CatalogItem from '@/components/catalog-item/catalog-item.vue';
+import {mapActions} from 'vuex';
+import {mapGetters} from 'vuex';
+
+
+export default {
+  name: 'catalog',
+  components: {
+    CatalogItem,
+  },
+  computed: {
+    ...mapGetters({
+      data: "DATA",
+      items: "ITEMS",
+    })
+  },
+  methods: {
+    ...mapActions([
+      "GET_DATA_FROM_API"
+    ])
+  },
+  mounted() {
+    this.GET_DATA_FROM_API()
+  }
+}
+</script>
